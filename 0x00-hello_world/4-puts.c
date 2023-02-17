efce5e47405c32e4130f0355942fd99611c141cb
adebayo aderinre james <jamesadebayo73@gmail.com>
2023-02-16 17:34:18 -0800

    hello, put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+
+/**
+ * main - A program that print a line with puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	put("\"programming is like building a multingual puzzle");
+	return (0);
+}
