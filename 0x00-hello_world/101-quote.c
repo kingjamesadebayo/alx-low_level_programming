f0ad9269978d8534165e7171ec87c418a77849c7
adebayo aderinre james <jamesadebayo73@gmail.com>
2023-02-16 20:54:07 -0800

    hello printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
 /**
- * main-program entry point
- * Return: Always 1 (success)
+ * main-program entry point.
+ *
+ * Return: 1 on success.
  */
+
 int main(void)
 {
-write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
