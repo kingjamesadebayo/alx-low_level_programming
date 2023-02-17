bfe95474d450818e91e15726e8623e127fee2a6a
adebayo aderinre james <jamesadebayo73@gmail.com>
2023-02-16 20:22:49 -0800

    hello printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
  * main-program entry point
  * Return: Always 1 (success)
  */
 int main(void)
 {
-	write(2,"and that piece of art is usefull\"-Dora Korpar,2015-10-19\n",59);
+	write(2,"and that piece of art is usefull\" -Dora Korpar,2015-10-19\n",59);
 	return (1);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 # include <stdio.h>
-
 /**
  * main - A program that prints a line using printf function
  * Return 0 (Success)
@@ -7,5 +6,5 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
