c25fdba332acc1d994edab4e87b3a92fbbfa36fc
adebayo aderinre james <jamesadebayo73@gmail.com>
2023-02-23 13:33:16 -0800

    0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,22 +11,23 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
-	
-	if (a > b && b > c)
-	
+
+	if (a > b && a > c)
 	{
 		largest = a;
 	}
-
-	else if (b > a && a > c)
+	else if (a > b && c > a)
+	{
+		largest = c;
+	}
+	else if (b > c)
 	{
 		largest = b;
 	}
-
 	else
 	{
 		largest = c;
 	}
-	
-	return (largest);
+
+return (largest);
 }
