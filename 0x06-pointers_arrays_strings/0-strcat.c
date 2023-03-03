f66b615106b42743ee4e9c6d055b5ebabcea3206
adebayo aderinre james <jamesadebayo73@gmail.com>
2023-03-02 18:52:12 -0800

    Mozart composed his music not for the elite, but for everybody

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _stcat - concatenates two strings
+ * _strcat - concatenates two strings
  * @dest: input value
  * @src: input value
  *
@@ -23,7 +23,7 @@ char *_strcat(char *dest, char *src)
 		i++;
 		j++;
 	}
+
 	dest[i] = '\0';
 	return (dest);
 }
-
--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * string_toupper - lowercase to uppercase
+ * string_toupper - change all lowercase to uppercase
  * @n: pointer
  *
  * Return: n
@@ -12,7 +12,7 @@ char *string_toupper(char *n)
 	i = 0;
 	while (n[i] != '\0')
 	{
-		if (n[i] >= 'a' && n[i] <= 'Z')
+		if (n[i] >= 'a' && n[i] <= 'z')
 			n[i] = n[i] - 32;
 		i++;
 	}
