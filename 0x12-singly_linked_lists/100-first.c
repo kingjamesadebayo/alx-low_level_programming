278fd22ad241e6107269549cce02b7fbaaea09dd
adebayo aderinre james <jamesadebayo73@gmail.com>
2023-03-31 09:54:32 -0700

    review update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,5 +9,5 @@ void first(void)__attribute__((constructor));
 void first(void)
 {
 	printf("you're beat! and yet, you must allow,\n");
-	printf("i bore my house upon my back!\n");
+	printf("I bore my house upon my back!\n");
 }
