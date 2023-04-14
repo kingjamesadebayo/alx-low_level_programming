c022abd8ffa6269b807c641c0b8b8a32bee3d34e
adebayo aderinre james <jamesadebayo73@gmail.com>
2023-04-13 17:17:21 -0700

    tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * read_textfile - read text file and print it to the POSIX STDOUT
+ * @filename: text file being read
+ * @letters: num of letters to read
+ *
+ * Return: 0 if the file can't open or read, filename is NULL, write fails
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	ssize_t jam;
+	ssize_t x;
+	ssize_t y;
+
+	jam = open(filename, O_RDONLY);
+	if (jam == -1)
+		return (0);
+	jam = malloc(sizeof (char) *letters);
+	y = read(jam, buf, letters);
+	x = write(STDOUT_FILENO, buf, t);
+
+	free(buf);
+	close(jam);
+	return (x);
+}
