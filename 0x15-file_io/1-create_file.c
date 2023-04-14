370931582783153b4a31aae239c50c860b901e2f
adebayo aderinre james <jamesadebayo73@gmail.com>
2023-04-13 19:27:12 -0700

    under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,9 +23,9 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		w = write(p, text_content, strlen(text_content));
-		if (w == -1)
+		if (w == 1)
 			close(p);
-		return (-1);
+		return (1);
 	}
 
 	close(p);
